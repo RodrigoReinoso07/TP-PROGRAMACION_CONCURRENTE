7cf65fd3446eb76ea1a8da0b2cb744e5ba56e6d2
RodrigoReinoso07 <rodrigoreinoso9@gmail.com>
2024-06-09 23:32:30 -0300

    ultima modificacion

--- src/test/Test.java
+++ src/test/Test.java
@@ -22,20 +22,28 @@ public class Test {
         System.out.println("ARREGLO DE 10 ELEMENTOS:");
         
         int arreglo1[] = generarArreglo(10);//GENERA ARREGLO DE 10 ELEMENTOS
+        int arreglo1c[] = arreglo1.clone();
         
         
+        System.out.println("Vector Original:");for (int i = 0; i < arreglo1.length; i++) {
+			System.out.print(arreglo1[i] + " ");}System.out.println("\n");
+			
         start = System.nanoTime();//CAPTURA TIEMPO INICIO
         ob.ordenar(arreglo1);//LLAMA AL ORDENAMIENTO SECUENCIAL
         end = System.nanoTime() - start;//CAPTURA TIEMPO FINAL
         
         System.out.println("EL ORDENAMIENTO DE ARREGLO DE 10 ELEMENTOS SECUENCIAL DEMORO: " + end + "ns");
         
-        longitudParte = arreglo1.length / numHilos;//ASIGNA LA CANTIDAD QUE CONTEMPLA CADA HILO
+        System.out.println("Vector Ordenado:");for (int i = 0; i < arreglo1.length; i++) {
+			System.out.print(arreglo1[i] + " ");}System.out.println("\n");
+        
+        longitudParte = arreglo1c.length / numHilos;//ASIGNA LA CANTIDAD QUE CONTEMPLA CADA HILO
         start = System.nanoTime();//CAPTURA TIEMPO INICIO
+        
         for (int i = 0; i < numHilos; i++) {
             int inicio = i * longitudParte;//ASIGNA INCIO CONTEMPLADO
-            int fin = (i == numHilos - 1) ? arreglo1.length - 1 : (inicio + longitudParte - 1);//ASIGNA FIN CONTEMPLADO
-            hilos[i] = new OrdenamientoBurbujaConcurrente(arreglo1, inicio, fin);//LLAMA AL ORDENAMIENTO
+            int fin = (i == numHilos - 1) ? arreglo1c.length - 1 : (inicio + longitudParte - 1);//ASIGNA FIN CONTEMPLADO
+            hilos[i] = new OrdenamientoBurbujaConcurrente(arreglo1c, inicio, fin);//LLAMA AL ORDENAMIENTO
             hilos[i].start();//LANZA EL HILO
         }
         try {
@@ -45,13 +53,15 @@ public class Test {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Arrays.sort(arreglo1);
+        Arrays.sort(arreglo1c);
         
         end = System.nanoTime() - start;//CAPTURA TIEMPO FINAL
         
         System.out.println("EL ORDENAMIENTO DE ARREGLO DE 10 CONCURRENTE ELEMENTOS DEMORO: " + end + "ns");
+        System.out.println("Vector Ordenado:");for (int i = 0; i < arreglo1c.length; i++) {
+			System.out.print(arreglo1c[i] + " ");}System.out.println("\n");
         
-        
+			
         //
         System.out.println("\n\n");
         //
@@ -60,7 +70,7 @@ public class Test {
         System.out.println("ARREGLO DE 1.000 ELEMENTOS:");
         
         int arreglo2[] = generarArreglo(1000);//GENERA ARREGLO DE 1000 ELEMENTOS
-        
+        int arreglo2c[] = arreglo2.clone();
         
         start = System.nanoTime();//CAPTURA TIEMPO INICIO
         ob.ordenar(arreglo2);//LLAMA AL ORDENAMIENTO SECUENCIAL
@@ -68,12 +78,12 @@ public class Test {
         
         System.out.println("EL ORDENAMIENTO DE ARREGLO DE 1.000 ELEMENTOS SECUENCIAL DEMORO: " + end + "ns");
         
-        longitudParte = arreglo2.length / numHilos;//ASIGNA LA CANTIDAD QUE CONTEMPLA CADA HILO
+        longitudParte = arreglo2c.length / numHilos;//ASIGNA LA CANTIDAD QUE CONTEMPLA CADA HILO
         start = System.nanoTime();// CAPTURA TIEMPO INICIO
         for (int i = 0; i < numHilos; i++) {
             int inicio = i * longitudParte;//ASIGNA INCIO CONTEMPLADO
-            int fin = (i == numHilos - 1) ? arreglo1.length - 1 : (inicio + longitudParte - 1);//ASIGNA FIN CONTEMPLADO
-            hilos[i] = new OrdenamientoBurbujaConcurrente(arreglo2, inicio, fin);//LLAMA AL ORDENAMIENTO
+            int fin = (i == numHilos - 1) ? arreglo2c.length - 1 : (inicio + longitudParte - 1);//ASIGNA FIN CONTEMPLADO
+            hilos[i] = new OrdenamientoBurbujaConcurrente(arreglo2c, inicio, fin);//LLAMA AL ORDENAMIENTO
             hilos[i].start();//LANZA EL HILO
         }
         try {
@@ -83,7 +93,7 @@ public class Test {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Arrays.sort(arreglo2);
+        Arrays.sort(arreglo2c);
         end = System.nanoTime() - start;//CAPTURA TIEMPO FINAL
         
         System.out.println("EL ORDENAMIENTO DE ARREGLO DE 1.000 CONCURRENTE ELEMENTOS DEMORO: " + end + "ns");
@@ -97,7 +107,7 @@ public class Test {
         System.out.println("ARREGLO DE 10.000 ELEMENTOS:");
         
         int arreglo3[] = generarArreglo(10000);//GENERA ARREGLO DE 10000 ELEMENTOS
-        
+        int arreglo3c[] = arreglo3.clone();
         
         start = System.nanoTime();//CAPTURA TIEMPO INICIO
         ob.ordenar(arreglo3);//LLAMA AL ORDENAMIENTO SECUENCIAL
@@ -105,12 +115,12 @@ public class Test {
         
         System.out.println("EL ORDENAMIENTO DE ARREGLO DE 10.000 ELEMENTOS SECUENCIAL DEMORO: " + end + "ns");
         
-        longitudParte = arreglo3.length / numHilos;//ASIGNA LA CANTIDAD QUE CONTEMPLA CADA HILO
+        longitudParte = arreglo3c.length / numHilos;//ASIGNA LA CANTIDAD QUE CONTEMPLA CADA HILO
         start = System.nanoTime();//CAPTURA TIEMPO INICIO
         for (int i = 0; i < numHilos; i++) {
             int inicio = i * longitudParte;//ASIGNA INCIO CONTEMPLADO
-            int fin = (i == numHilos - 1) ? arreglo3.length - 1 : (inicio + longitudParte - 1);//ASIGNA FIN CONTEMPLADO
-            hilos[i] = new OrdenamientoBurbujaConcurrente(arreglo3, inicio, fin);//LLAMA AL ORDENAMIENTO
+            int fin = (i == numHilos - 1) ? arreglo3c.length - 1 : (inicio + longitudParte - 1);//ASIGNA FIN CONTEMPLADO
+            hilos[i] = new OrdenamientoBurbujaConcurrente(arreglo3c, inicio, fin);//LLAMA AL ORDENAMIENTO
             hilos[i].start();//LANZA EL HILO
         }
         try {
@@ -120,7 +130,7 @@ public class Test {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Arrays.sort(arreglo3);
+        Arrays.sort(arreglo3c);
         end = System.nanoTime() - start;
         
         System.out.println("EL ORDENAMIENTO DE ARREGLO DE 10.000 CONCURRENTE ELEMENTOS DEMORO: " + end + "ns");
@@ -134,7 +144,7 @@ public class Test {
         System.out.println("ARREGLO DE 100.000 ELEMENTOS:");
         
         int arreglo4[] = generarArreglo(100000);//GENERA ARREGLO DE 100000 ELEMENTOS
-        
+        int arreglo4c[] = arreglo4.clone();
         
         start = System.nanoTime();//CAPTURA TIEMPO INICIO
         ob.ordenar(arreglo4);//LLAMA AL ORDENAMIENTO SECUENCIAL
@@ -142,12 +152,12 @@ public class Test {
         
         System.out.println("EL ORDENAMIENTO DE ARREGLO DE 100.000 ELEMENTOS SECUENCIAL DEMORO: " + end + "ns");
         
-        longitudParte = arreglo4.length / numHilos;//ASIGNA LA CANTIDAD QUE CONTEMPLA CADA HILO
+        longitudParte = arreglo4c.length / numHilos;//ASIGNA LA CANTIDAD QUE CONTEMPLA CADA HILO
         start = System.nanoTime();
         for (int i = 0; i < numHilos; i++) {
             int inicio = i * longitudParte;//ASIGNA INCIO CONTEMPLADO
-            int fin = (i == numHilos - 1) ? arreglo4.length - 1 : (inicio + longitudParte - 1);//ASIGNA FIN CONTEMPLADO
-            hilos[i] = new OrdenamientoBurbujaConcurrente(arreglo4, inicio, fin);//LLAMA AL ORDENAMIENTO
+            int fin = (i == numHilos - 1) ? arreglo4c.length - 1 : (inicio + longitudParte - 1);//ASIGNA FIN CONTEMPLADO
+            hilos[i] = new OrdenamientoBurbujaConcurrente(arreglo4c, inicio, fin);//LLAMA AL ORDENAMIENTO
             hilos[i].start();//LANZA EL HILO
         }
         try {
@@ -157,7 +167,7 @@ public class Test {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Arrays.sort(arreglo4);
+        Arrays.sort(arreglo4c);
         end = System.nanoTime() - start;//CAPTURA TIEMPO FINAL
         
         System.out.println("EL ORDENAMIENTO DE ARREGLO DE 100.000 CONCURRENTE ELEMENTOS DEMORO: " + end + "ns");
